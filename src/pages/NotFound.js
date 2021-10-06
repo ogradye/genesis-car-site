bef899ff67fea38e28a01e86b40c35a373bac4bf
Ethan <69441307+ogradye@users.noreply.github.com>
2021-10-05 21:58:29 -0400

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound(){
 	return (
-		<div className="container">
-			<h1>404 not found</h1>
+		<div className="container nf-c">
+			<img src="/logo.png" alt="Genesis logo"></img>
+			<h1>404 - Not Found</h1>
+			<p>We couldn't find what you were looking for. <br></br> It may have been deleted or never exsisted in the first place.</p>
+			<Link to='/' className="btn btn-primary">Go Back Home</Link>
 		</div>
 	)
 }
